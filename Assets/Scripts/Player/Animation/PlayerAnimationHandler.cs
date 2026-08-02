61f5b11014db0bd8e9cd7c003ab0cbf54e1fb968
Kryton Fox Night <krytonfoxnight@gmail.com>
2026-08-02 18:18:33 +0900

    refactor: player animation handler로 중앙화 및 피격 애니메이션 연결

---
+++ Assets/Scripts/Player/Animation/PlayerAnimationHandler.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace Player.Animation
+{
+    public class PlayerAnimationHandler : MonoBehaviour
+    {
+        [SerializeField] private Animator animator;
+
+        public void SetTrigger(PlayerAnimationStatus status) => animator.SetTrigger(status.GetAnimatorHash());
+
+        public void SetBool(PlayerAnimationStatus status, bool value) => animator.SetBool(status.GetAnimatorHash(), value);
+
+        public void SetFloat(PlayerAnimationStatus status, float value) => animator.SetFloat(status.GetAnimatorHash(), value);
+
+        public void SetFloat(PlayerAnimationStatus status, float value, float dampTime, float deltaTime) =>
+            animator.SetFloat(status.GetAnimatorHash(), value, dampTime, deltaTime);
+    }
+}
--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -1,10 +1,13 @@
 using Combat;
+using Player.Animation;
 using UnityEngine;
 
 namespace Player
 {
     public class PlayerHealth : MonoBehaviour, IDamageable
     {
+        [SerializeField] private PlayerAnimationHandler animationHandler;
+
         [SerializeField] private int maxHp = 100;
 
         private Health _health;
@@ -17,6 +20,7 @@ namespace Player
         public void TakeDamage(int amount)
         {
             var applied = _health.ApplyDamage(amount);
+            animationHandler.SetTrigger(PlayerAnimationStatus.GetHit);
             Debug.Log($"Player took {applied} damage");
         }
     }
